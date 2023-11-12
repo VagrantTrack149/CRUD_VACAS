af0fdea9b92e3b287f65030a4ccc0c1128bf78c2
Neil Otniel Moreno Rivera <neilotniel@gmail.com>
2023-11-12 15:01:01 -0600

    Frontend avance

--- www/Ganadero.php
+++ www/Ganadero.php
@@ -8,23 +8,45 @@
 <link rel="stylesheet" href="style/Control.css">
 <div class="container d-flex">
 
-<div class="col-md-6 flex-fill">
+<div class="col-md-3 justify-content-start">
   <div class="card">
     <div class="card-header">
       <h3 class="card-title">Menu de Ganaderos</h3>
     </div>
     <div class="card-body">
       <div class="form-group">
-        <label for="campo1">Descripción</label>
+        <label for="campo1">PSG</label>
         <input type="text" class="form-control" id="campo1" name="campo1">
       </div>
       <div class="form-group">
-        <label for="campo2">Cantidad</label>
+        <label for="campo2">Nombre Completo</label>
         <input type="text" class="form-control" id="campo2" name="campo2">
       </div>
       <div class="form-group">
-        <label for="campo3">Precio</label>
+        <label for="campo3">Rancho</label>
         <input type="text" class="form-control" id="campo3" name="campo3">
+      </div>
+      <div class="form-group">
+        <label for="campo3">Domicilio</label>
+        <input type="text" class="form-control" id="campo3" name="campo3">
+      </div>
+      <div class="form-group">
+        <label for="campo3">Localidad</label>
+        <input type="text" class="form-control" id="campo3" name="campo3">
+      </div>
+      <div class="form-group">
+        <label for="campo3">Municipio</label>
+        <input type="text" class="form-control" id="campo3" name="campo3">
+      </div>
+      <div class="form-group">
+        <label for="campo3">Estado</label>
+        <input type="text" class="form-control" id="campo3" name="campo3">
+      </div>
+      <div class="form-group">
+      <select class="form-select form-select-lg mb-3" aria-label="Small select example">
+          <option selected>Externo</option>
+          <option selected>Trabajador(interno)</option>
+        </select>
       </div>
       <button type="submit" class="btn btn-primary">Añadir</button>
       <button type="submit" class="btn btn-secondary">Lista de Medicamento</button>
@@ -32,7 +54,7 @@
   </div>
 </div>
 
-<div class="col-md-6 flex-fill">
+<div class="col-md-12 flex-fill">
 <div class="row">
       <div class="col-md-12">
         <div class="input-group mb-3">
@@ -47,16 +69,25 @@
   <table class="table table-bordered">
     <thead>
       <tr>
-        <th>Etapa</th>
-        <th>Recetas</th>
+        <th>PSG</th>
+        <th>Nombre Completo</th>
+        <th>Rancho</th>
+        <th>Domicilio</th>
+        <th>Localidad</th>
+        <th>Municipio</th>
+        <th>Estado</th>
         <th>Opciones</th>
-        
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td>1</td>
-        <td><h8> Receta</h4></td>
+        <td>PSG</td>
+        <td><h8> NOMBRE</h4></td>
+        <td>Ranchito</td>
+        <td>Domicilio</td>
+        <td>Localidad</td>
+        <td>Municipio</td>
+        <td>Estado</td>
         <td><button class="btn btn-success apply"><i class="fa-solid fa-shield-virus"></i>
         <button class="btn btn-primary edit"><i class="fa-solid fa-pen-to-square"></i></button>
         <button class="btn btn-danger delete"><i class="fa-solid fa-trash"></i></button></button></td>
--- www/Ganado.php
+++ www/Ganado.php
@@ -6,65 +6,34 @@
     }
 ?>   
 <link rel="stylesheet" href="style/Control.css">
-<div class="container d-flex">
-
-<div class="col-md-6 flex-fill">
-  <div class="card">
-    <div class="card-header">
-      <h3 class="card-title">Menu de Ganado y lotes</h3>
-    </div>
-    <div class="card-body">
-      <div class="form-group">
-        <label for="campo1">Descripción</label>
-        <input type="text" class="form-control" id="campo1" name="campo1">
-      </div>
-      <div class="form-group">
-        <label for="campo2">Cantidad</label>
-        <input type="text" class="form-control" id="campo2" name="campo2">
-      </div>
-      <div class="form-group">
-        <label for="campo3">Precio</label>
-        <input type="text" class="form-control" id="campo3" name="campo3">
-      </div>
-      <button type="submit" class="btn btn-primary">Añadir</button>
-      <button type="submit" class="btn btn-secondary">Lista de Medicamento</button>
-    </div>
-  </div>
-</div>
-
-<div class="col-md-6 flex-fill">
-<div class="row">
+<div class="container mx-auto text-center Taco">
+  <div class="flex-fill">
+    <div class="row">
       <div class="col-md-12">
         <div class="input-group mb-3">
           <input type="text" class="form-control" placeholder="Buscar" aria-label="Buscar">
           <div class="input-group-append">
             <button class="btn btn-primary" type="button"><i class="fa-solid fa-magnifying-glass"></i></button>
-            <button class="btn btn-success" type="button"><i class="fa-solid fa-capsules"></i><i class="fa-solid fa-plus"></i></button>
+            <button class="btn btn-success" type="button"><i class="fa-solid fa-cow"></i><i class="fa-solid fa-plus"></i></button>
           </div>
         </div>
       </div>
     </div>
-  <table class="table table-bordered">
-    <thead>
-      <tr>
-        <th>Etapa</th>
-        <th>Recetas</th>
-        <th>Opciones</th>
-        
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>1</td>
-        <td><h8> Receta</h4></td>
-        <td><button class="btn btn-success apply"><i class="fa-solid fa-shield-virus"></i>
-        <button class="btn btn-primary edit"><i class="fa-solid fa-pen-to-square"></i></button>
-        <button class="btn btn-danger delete"><i class="fa-solid fa-trash"></i></button></button></td>
-      </tr>
-      
-    </tbody>
-  </table>
-</div>
-
-</div>
-  <?php include 'Template/footer.php'; ?>
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th>Lote</th>
+          <th>Cantidad de animales</th>
+          <th>Opciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td><h8> Contador</h8></td>
+          <td><button class="btn btn-success apply"><i class="fa-solid fa-cow"></i></button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>  <?php include 'Template/footer.php'; ?>
