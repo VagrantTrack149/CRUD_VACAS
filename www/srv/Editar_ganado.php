f3b30d8eeb6e1eb50f955803cb4c8ed992f88724
Neil Otniel Moreno Rivera <neilotniel@gmail.com>
2023-11-25 18:33:51 -0600

    Logica granja funcional p2 correción de errores

--- www/srv/Editar_ganado.php
+++ www/srv/Editar_ganado.php
@@ -17,19 +17,18 @@
         }
         if (isset($_POST['edit'])){
             $id=$_GET['id'];
-            $sex= $row['Sexo'];
-            $edad=$row['Edad'];
-            $lote=$row['Lote'];
-            $peso=$row['Peso'];
-            $estado=$row['Estado'];
-            $precio=$row['Precio'];
-            #$query= "UPDATE Productos SET Producto='$producto', Cantidad=$cantidad, Precio=$precio WHERE idProducto= $id";
+            $sex= $_GET['Sexo'];
+            $edad=$_GET['Edad'];
+            $lote=$_GET['Lote'];
+            $peso=$_GET['Peso'];
+            $estado=$_GET['Estado'];
+            $precio=$_GET['Precio'];
             $query= "UPDATE Ganado SET No_Arete='$id',Sexo=$sex,Edad=$edad,Lote=$lote,Peso=$peso,Estado='$estado',Precio=$precio WHERE Ganado.No_Arete='$id'";
             $result= mysqli_query($conn,$query);    
-            #$_SESSION['Message'] ='Producto editado con exito';
-            #$_SESSION['Message_type'] ='warning';
+            #$_SESSION['Message'] ='Registro del animal editado con exito';
+            #$_SESSION['Message_type'] ='Success';
             echo "<script>window.location.href='../Ganado.php';</script>";
-            #header("location:Editar_ganado.php");
+            #header("location:Editar_ganado.php"); #quitar
         }
     }
 ?>
@@ -37,29 +36,29 @@
     <div class="row">
         <div class="col-md-4 mx-auto">
             <div class="card card-body">
-                <form action="Editar_ganado.php?id=<?php echo $_GET['id'];?>" method="POST">
+                <form action="Editar_ganado.php?id=<?php echo $_GET['No_Arete'];?>" method="POST">
                 <div class="from-group">
                         <input type="text" name="Numero de arete" value="<?php   echo $id;    ?>" class="from-control" placeholder="Numero de arete">
                     </div>
                     <div class="from-group">
-                        <input type="text" name="Sexo" value="<?php   echo $sex;    ?>" class="from-control" placeholder="Sexo">
+                        <input type="text" name="Sexo" value="<?php   echo $sex;    ?>" class="from-control" placeholder="Sexo 1-MACHO 0-HEMBRA">
                     </div>
                     <div class="from-group">
                         <input type="text" name="Edad" value="<?php   echo $edad;   ?>" class="from-control" placeholder="Edad(meses)">
                     </div>
                     <div class="from-group">
-                        <input type="text" name="lote" value="<?php   echo $lote;   ?>" class="from-control" placeholder="Edad(meses)">
+                        <input type="text" name="Lote" value="<?php   echo $lote;   ?>" class="from-control" placeholder="Edad(meses)">
                     </div>
                     <div class="from-group">
-                        <input type="text" name="peso" value="<?php   echo $peso;   ?>" class="from-control" placeholder="Peso(kg)">
+                        <input type="text" name="Peso" value="<?php   echo $peso;   ?>" class="from-control" placeholder="Peso(kg)">
                     </div>
                     <div class="from-group">
-                        <input type="text" name="estado" value="<?php   echo $estado;   ?>" class="from-control" placeholder="Engorda, Defunsión, etc">
+                        <input type="text" name="Estado" value="<?php   echo $estado;   ?>" class="from-control" placeholder="Engorda, Defunsión, etc">
                     </div>
                     <div class="from-group">
-                        <input type="text" name="precio" value="<?php   echo $precio;   ?>" class="from-control" placeholder="($)Precio">
+                        <input type="text" name="Precio" value="<?php   echo $precio;   ?>" class="from-control" placeholder="($)Precio">
                     </div>
-                    <input type="submit" class="btn btn-success btn-block" name="edit" value="Actualizar ganado">
+                        <input type="submit" class="btn btn-success btn-block" name="edit" value="Actualizar ganado">
                     </div>
                 </form>
             </div>
